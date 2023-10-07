532538b5ae701db7da54290cd59ff0f51ebe7310
Iceneo <nosminin@rit-it.com>
2023-10-07 16:18:22 +0500

    initial commit

---
+++ src/main/java/com/example/spring_proxy/SpringProxyApplication.java
@@ -0,0 +1,13 @@
+package com.example.spring_proxy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringProxyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringProxyApplication.class, args);
+    }
+
+}
